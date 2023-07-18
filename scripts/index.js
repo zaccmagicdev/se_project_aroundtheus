d20ee70cfed1915e11916b4ab36856e81d38b85e
Zachary Wedel <zwede710@gmail.com>
2023-07-17 20:53:40 -0500

    UPDATE: Sprint 5 additions

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+//our initial array
 const initialCards = [{
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -18,26 +19,55 @@ const initialCards = [{
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
 }];
 
+//variables we need
 const editButton = document.querySelector('#edit-button');
 const addImageButton = document.querySelector('.profile__add-button');
 const profileModalBox = document.querySelector('#profile-modal-box');
 const addImageModalBox = document.querySelector('#add-modal-box');
+const enlargedModalBox = document.querySelector('#enlarged-modal-box');
+const enlargedModalImg = enlargedModalBox.querySelector('.modal__enlarged-image');
+const enlargedModalTxt = enlargedModalBox.querySelector('.modal__enlarged-text');
+const enlargedModalExitButton = enlargedModalBox.querySelector('.modal__exit-button');
 const profileModalExitButton = profileModalBox.querySelector('.modal__exit-button');
 const addImageModalExitButton = addImageModalBox.querySelector('.modal__exit-button');
 const profileModalSaveButton = document.querySelector('#profile-modal-save-button');
-const addImageModalSaveButton = document.querySelector('#add-modal-save-button');
+const addImageModalCreateButton = document.querySelector('#add-modal-create-button');
 const userName = document.querySelector('#user-name');
 const userCareer = document.querySelector('#user-career');
 const userNameInput = document.querySelector('#profile-modal__input-name');
 const userDescInput = document.querySelector('#profile-modal__input-description');
+const userImageTitle = addImageModalBox.querySelector('#add-modal__input-name');
+const userImageLink = addImageModalBox.querySelector('#add-modal__input-link');
 const profileEditForm = document.querySelector('#profile__modal-form');
+const addImageEditForm = document.querySelector('#add-image__modal-form');
 const cardTemplate = document.querySelector('#card-template').content.firstElementChild;
 const cardListElements = document.querySelector('.cards');
 
+
+//section for required functions
 function getCardElement(cardData){
     const cardElement = cardTemplate.cloneNode(true);
     let cardCaption = cardElement.querySelector('.card__caption');
     let cardImage = cardElement.querySelector('.card__image');
+    const cardLikeButton = cardElement.querySelector('.card__like-button');
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    const cardImageButton = cardElement.querySelector('#card-image-button');
+
+    cardImageButton.addEventListener("click", () => {
+        enlargedModalImg.setAttribute('src', cardData.link);
+        enlargedModalImg.setAttribute('alt', cardData.name);
+        enlargedModalTxt.textContent = cardData.name;
+
+        openModal(enlargedModalBox);
+    });
+
+    cardLikeButton.addEventListener("click", () => {
+        cardLikeButton.classList.toggle('card__like-button_clicked');
+    });
+
+    cardDeleteButton.addEventListener("click", () => {
+        cardElement.remove();
+    });
 
     cardCaption.textContent = cardData.name;
     cardImage.setAttribute('src', cardData.link);
@@ -54,6 +84,7 @@ function closeModal(modal){
     modal.classList.remove('modal__opened')
 }
 
+//section for event handlers
 editButton.addEventListener("click", () => {
     userNameInput.setAttribute("value", userName.textContent);
     userDescInput.setAttribute("value", userCareer.textContent);
@@ -72,6 +103,10 @@ addImageModalExitButton.addEventListener("click", () => {
     closeModal(addImageModalBox);
 });
 
+enlargedModalExitButton.addEventListener("click", () => {
+    closeModal(enlargedModalBox);
+});
+
 profileEditForm.addEventListener("submit", (e) => {
     e.preventDefault();
     userName.textContent = userNameInput.value;
@@ -79,6 +114,26 @@ profileEditForm.addEventListener("submit", (e) => {
     closeModal(profileModalBox);
 });
 
+addImageEditForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = userImageTitle.value;
+    const link = userImageLink.value;
+
+    if(!link.includes("https")){
+        alert("Please enter a valid image link!");
+    } else {
+
+    const newCard = getCardElement({
+        name,
+        link,
+    });
+
+    cardListElements.prepend(newCard);
+    closeModal(addImageModalBox);
+    }
+});
+
+//initializing and adding our cards to the list
 initialCards.forEach((cardData) => {
    const cardElement = getCardElement(cardData);
    cardListElements.append(cardElement);
