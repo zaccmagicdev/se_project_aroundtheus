8e0c2fd4c5131925de12b1ed3d91a2577734af59
Zachary Wedel <zwede710@gmail.com>
2023-09-29 14:09:36 -0500

    UPDATE: continuing sprint 9

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,13 @@ import {
     addImageButton,
     profileEditForm,
     addImageEditForm,
+    editAvatarForm,
     profileEditNameInput,
     profileEditJobInput,
     editAvatarButton,
-    avatarPicture
+    avatarPicture,
+    avatarJob,
+    avatarName
 }
     from "../utils/constants.js";
 
@@ -38,35 +41,39 @@ function setProfilePicture(){
 }
 
 function setProfile(){
-    //api.getUserData().then(res => )
+    api.getUserData().then((res) => {
+        avatarJob.textContent = res.about;
+        avatarName.textContent = res.name;
+    });
 }
 
+//saving data
 setProfilePicture();
+setProfile();
 
 //modal related code
 const userInfo = new UserInfo('#user-name', '#user-career');
 const enlargeImageModal = new PopUpWithImage('#enlarged-modal-box');
 const confirmationModal = new Popup('#confirm-modal');
 const editAvatarModal = new PopUpWithForm('#avatar-edit-modal', (data) =>{
-    //first welll make the request and use whatever is submitted as the link ofc
     api.updateProfilePic(data["avatar-link"]);
     setProfilePicture();
     editAvatarModal.close();
-
     //we will also intermittnely add a method call here for the 'Saving', same will go for uploading cards
 });
 const addImageModal = new PopUpWithForm("#add-modal-box", (data) => {
-    cardList.prependItem(createCard({
+    api.uploadCard(data["add-img-title"], data["add-img-link"])
+    .then(cardList.prependItem(createCard({
         name: data["add-img-title"],
         link: data["add-img-link"]
-    }));
+    })))
     addImageModal.close();
 });
 
 const editProfileModal = new PopUpWithForm("#profile-modal-box", (data) => {
     api.setProfileData
     (data["profile-modal-username"], data["profile-modal-desc"]);
-    
+    setProfile();
     editProfileModal.close(); 
 });
 
@@ -87,28 +94,26 @@ function createCard(item) {
 }
 
 //FOR RENDERING CARDS: we will take whatever from the array of cards we get, use that array as it's card count, have link and name come from the server side cards that we uploaded
+    //we will need to find a way for the loaded cards to be accessed through out the entire program
 
 const cardList = new Section({
-    items: initialCards,
-    renderer: (item) => {
-        cardList.addItem(createCard(item));
-    }
+    items: [],
+    renderer: () => {}
 }, '.cards');
 
 cardList.renderItems();
 
 const addFormValidator = new FormValidator(formConfig, addImageEditForm);
 const editFormValidator = new FormValidator(formConfig, profileEditForm);
-//const avatarFormValidator = new FormValidator(formConfig, confirmationModal);
+const avatarFormValidator = new FormValidator(formConfig, editAvatarForm);
 
 addFormValidator.enableValidation();
 editFormValidator.enableValidation();
-//avatarFormValidator.enableValidation();
+avatarFormValidator.enableValidation();
 
 //section for event handlers
 editButton.addEventListener("click", () => {
     editProfileModal.open();
-   
     const { name, job } = userInfo.getUserInfo();
     profileEditNameInput.value = name;
     profileEditJobInput.value = job;
